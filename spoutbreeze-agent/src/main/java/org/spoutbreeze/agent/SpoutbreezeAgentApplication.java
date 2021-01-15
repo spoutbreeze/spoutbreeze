5974b6a944e510a2848d1e94e410d38d7825464c
Ghazi Triki <ghazi.triki@riadvice.tn>
2021-01-15 18:13:26 +0100

    Add spoutbreeze-agent project.

---
+++ spoutbreeze-agent/src/main/java/org/spoutbreeze/agent/SpoutbreezeAgentApplication.java
@@ -0,0 +1,13 @@
+package org.spoutbreeze.agent;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpoutbreezeAgentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpoutbreezeAgentApplication.class, args);
+	}
+
+}
