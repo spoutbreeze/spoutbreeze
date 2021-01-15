583b85c24c01feb5c76741fa71098bc760f8cd2d
Ghazi Triki <ghazi.triki@riadvice.tn>
2021-01-15 18:13:48 +0100

    Add spoutbreeze-manager project.

---
+++ spoutbreeze-manager/src/main/java/org/spoutbreeze/spoutbreezemanager/SpoutbreezeManagerApplication.java
@@ -0,0 +1,13 @@
+package org.spoutbreeze.spoutbreezemanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpoutbreezeManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpoutbreezeManagerApplication.class, args);
+	}
+
+}
---
+++ spoutbreeze-manager/src/test/java/org/spoutbreeze/spoutbreezemanager/SpoutbreezeManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package org.spoutbreeze.spoutbreezemanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpoutbreezeManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
