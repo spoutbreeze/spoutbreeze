49604aefc5743b28100f3c37a0cd95d07e5eb481
Ghazi Triki <ghazi.triki@riadvice.com>
2026-09-04 10:07:43 +0100

    Retire the 2021 build scripts, broadcaster and Selenoid grid.

--- spoutbreeze-broadcaster/build.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-sudo docker build -t spoutbreeze/spoutbreeze-broadcaster:0.1 .
--- spoutbreeze-selenoid/run.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-sudo docker stop $(sudo docker ps -a -q)
-sudo docker rm $(sudo docker ps -a -q)
-sudo docker-compose rm -f
-sudo docker-compose up -d
